1898dc6d2c3df5af795ab1d260f4e5393420590e
Neevesh Jain <neeveshjain27@gmail.com>
2025-11-23 00:10:18 +0530

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Neevesh Jain on 23/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
